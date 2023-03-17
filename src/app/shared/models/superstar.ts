ae90bc67addacaed28a5eba3d94214698382dea0
Norman <norman@wamberchies.be>
2023-03-17 16:18:13 +0100

    update

--- src/app/shared/models/superstar.ts
+++ src/app/shared/models/superstar.ts
@@ -20,6 +20,8 @@ export interface superstar{
 
   heel: boolean;
 
+  image: string;
+
 }
 
 // "id": 1,
--- src/app/shared/services/superstar.service.ts
+++ src/app/shared/services/superstar.service.ts
@@ -8,8 +8,6 @@ import {superstar} from "../models/superstar";
 export class SuperstarService {
 
   constructor(private _httpClient : HttpClient) { }
-
-
   getAllSuperstars(): any {
     return this._httpClient.get('http://localhost:8080/superstar/all');
   }
